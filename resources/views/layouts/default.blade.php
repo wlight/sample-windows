2080dd7e23464a6498285e1a1ff44ffc7a0a161f
wlight <wormside@gmail.com>
2017-06-14 11:18:12 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,26 @@
 <!doctype html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Sample App')</title>
-</head>
-<body>
-    @yield('content')
-</body>
+    <head>
+        <link rel="stylesheet" href="/css/app.css">
+        <title>@yield('title', 'Sample App')</title>
+    </head>
+    <body>
+        <header class="navbar navbar-fixed-top navbar-inverse">
+            <div class="container">
+                    <div class="col-md-offset-1 col-md-10">
+                    <a href="/" id="logo">Sample App</a>
+                    <nav>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="/help">帮助</a></li>
+                            <li><a href="#">登录</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            @yield('content')
+        </div>
+
+    </body>
 </html>
